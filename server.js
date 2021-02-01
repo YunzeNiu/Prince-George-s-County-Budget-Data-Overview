05f60d7f772020e044252e66c2c93a0500d3e46d
YunzeNiu <nnnyyyzzz731@gmail.com>
2021-01-31 21:32:54 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+// These are our required libraries to make the server work.
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import express from 'express';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+import { open } from 'sqlite';
+import sqlite3 from 'sqlite3';
+import { createRequire } from 'module';
+import path from 'path';
+const __dirname = path.resolve();
+
+
+const require = createRequire(import.meta.url);
+
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.route('/api')
+  .get(async (req, res) => {
+    console.log('GET request detected');
+  })
+  .post(async (req, res) => {
+    console.log('POST request detected');
+    console.log('Form data in res.body', req.body);
+  });
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+
